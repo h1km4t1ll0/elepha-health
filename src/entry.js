b8397d34250d4ecfedffa2ccb117f28b71fa4b2e
Pavel Zelenin <murdock8@mail.ru>
2023-12-10 22:02:45 +0700

    Revert "necessary files"
    This reverts commit de816f1836726e0b625de989bb3eeae837482f9b.

--- src/entry.js
+++
@@ -1,9 +0,0 @@
-import { registerRootComponent } from 'expo';
-import App from 'app/index';
-import { NativeWindStyleSheet } from "nativewind";
-
-NativeWindStyleSheet.setOutput({
-  default: "native",
-});
-
-registerRootComponent(App);
